79caf5e97d2df14db714e2d21dafa6273d36328d
Max William <83377057+maxwsj@users.noreply.github.com>
2022-06-12 22:05:31 -0300

    Initial commit

---
+++ Seção 2 - Introdução ao C# e .Net/WS-VS-2019/PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,15 @@
+﻿using System; // Aponta (Referencia) para um namespace chamado System, sendo um namespace padrão do .Net. Contém funções básicas de manipulação de programa
+
+// Por padrão o VS declara nosso namespace com o mesmo nome do Projeto. (Podemos colocar o nome que quisermos)
+namespace PrimeiroProjeto // Declaração de namespace do nosso arquivo.
+// Nosso programa também pode ser parte de um namespace
+{
+    class Program // Por convenção, colocamos o nome da classe com o mesmo nome do arquivo (Porém não é obrigatório)
+    {
+        // Dentro da classe podemos ter membros da classe
+        static void Main(string[] args) // Identificação padrão para identificar o entry point (onde a execução começa)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Seção 3 - Recaptulação de Lógica de Programação Usando C#/Built-in data types/BuiltInDataTypes/BuiltInDataTypes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BuiltInDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool completo = false;
+            char genero = 'F';
+            // \u : Informa que será um código unicode
+            char letra = '\u0046'; // código unicode de caracteres | Chars são representados por aspas simples
+            byte n1 = 126;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483648L; // Quando trabalhamos com long, é recomendado colocar o L no final para deixar explícito o tipo long.
+            double n5 = 4.5; // Double é com ponto (por padrão do C# será exibido com vírgula na minha tela pois ele segue o padrão de pontuação da máquina)
+            float n6 = 4.5f; // Float devemos colocar o f no final para informar explicitamente que se trata de um float
+            string nome = "Max William Santos de Jesus"; // Strings são representadas por aspas duplas
+            nome = "Andressa  Gomes"; // Isso pode acontecer pois estamos atribuindo um novo valor a string e não modificando-a (em sua essência)
+            object obj1 = "Maxuel Alves"; // É um tipo genêrico
+
+            ///////////////////////////////////////////////////////
+            ///// Funções para valores mínimos e máximos /////
+            int n7 = int.MinValue;
+            int n8 = int.MaxValue;
+
+
+        }
+    }
+}
